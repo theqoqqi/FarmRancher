4d424abac030ba94f6ebaccc7d7592440de26d9e
Qoqqi <sazykin.georgii@gmail.com>
2023-10-28 19:26:40 +0300

    feat: Добавлен ReplacePlantLootModifier

--- src/main/java/ru/qoqqi/farmrancher/common/loot/GlobalLootModifiers.java
+++ src/main/java/ru/qoqqi/farmrancher/common/loot/GlobalLootModifiers.java
@@ -6,6 +6,7 @@ import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import ru.qoqqi.farmrancher.FarmRancher;
 
@@ -13,6 +14,9 @@ public class GlobalLootModifiers {
 
 	public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> GLMS =
 			DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, FarmRancher.MOD_ID);
+
+	public static final RegistryObject<Codec<ReplacePlantLootModifier>> REPLACE_PLANT_LOOT =
+			GLMS.register("replace_plant_loot", () -> ReplacePlantLootModifier.CODEC);
 
 	public static void register(IEventBus eventBus) {
 		GLMS.register(eventBus);
---
+++ src/main/java/ru/qoqqi/farmrancher/common/loot/ReplacePlantLootModifier.java
@@ -0,0 +1,63 @@
+package ru.qoqqi.farmrancher.common.loot;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.LootModifier;
+
+import javax.annotation.Nonnull;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import ru.qoqqi.farmrancher.common.plants.Plants;
+
+public class ReplacePlantLootModifier extends LootModifier {
+
+	public static Codec<ReplacePlantLootModifier> CODEC = RecordCodecBuilder.create(
+			instance -> instance
+					.group(
+							LOOT_CONDITIONS_CODEC.fieldOf("conditions").forGetter(lm -> lm.conditions)
+					)
+					.apply(instance, ReplacePlantLootModifier::new)
+	);
+
+	protected ReplacePlantLootModifier(LootItemCondition[] conditionsIn) {
+		super(conditionsIn);
+	}
+
+	@Nonnull
+	@Override
+	protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+		if (shouldReplaceLoot(context)) {
+			generatedLoot.clear();
+		}
+
+		return generatedLoot;
+	}
+
+	private static boolean shouldReplaceLoot(LootContext context) {
+		var blockState = context.getParamOrNull(LootContextParams.BLOCK_STATE);
+		var position = context.getParamOrNull(LootContextParams.ORIGIN);
+
+		if (blockState == null || position == null) {
+			return false;
+		}
+
+		var plant = Plants.get(blockState);
+
+		if (plant == null) {
+			return false;
+		}
+
+		return plant.type.isMaxAge(blockState);
+	}
+
+	@Override
+	public Codec<? extends IGlobalLootModifier> codec() {
+		return CODEC;
+	}
+}
