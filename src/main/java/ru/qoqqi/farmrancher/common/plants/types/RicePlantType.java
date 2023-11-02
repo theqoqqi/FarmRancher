f6c127c7a27f0837a97825cffe834c29229ab4ad
Qoqqi <sazykin.georgii@gmail.com>
2023-11-03 00:09:51 +0300

    feat: Добавлен новый тип растений RicePlantType

---
+++ src/main/java/ru/qoqqi/farmrancher/common/plants/types/RicePlantType.java
@@ -0,0 +1,68 @@
+package ru.qoqqi.farmrancher.common.plants.types;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import vectorwing.farmersdelight.common.block.RiceBlock;
+import vectorwing.farmersdelight.common.block.RicePaniclesBlock;
+
+public class RicePlantType extends AgedPlantType<RiceBlock> {
+
+	private final RicePaniclesBlock paniclesBlock;
+
+	public RicePlantType(RiceBlock block, RicePaniclesBlock paniclesBlock) {
+		super(block);
+
+		this.paniclesBlock = paniclesBlock;
+	}
+
+	@Override
+	public int getAge(BlockState state) {
+		return state.getValue(RiceBlock.AGE);
+	}
+
+	@Override
+	public int getMaxAge() {
+		return block.getMaxAge();
+	}
+
+	@Override
+	protected BlockState getStateForAge(RiceBlock block, int age) {
+		return block.withAge(age);
+	}
+
+	@Override
+	public void growCrops(Level level, BlockPos pos) {
+		if (hasPlantAt(level, pos) && isMaxAge(level, pos)) {
+			spawnPaniclesFor(level, pos);
+			return;
+		}
+
+		super.growCrops(level, pos);
+	}
+
+	private void spawnPaniclesFor(Level level, BlockPos pos) {
+		var paniclesPos = pos.above();
+
+		if (!canSpawnOn(level, paniclesPos)) {
+			return;
+		}
+
+		spawnPanicles(level, paniclesPos);
+	}
+
+	private void spawnPanicles(Level level, BlockPos paniclesPos) {
+		var state = paniclesBlock.defaultBlockState();
+
+		level.setBlockAndUpdate(paniclesPos, state);
+	}
+
+	private boolean canSpawnOn(Level level, BlockPos pos) {
+		if (!level.isEmptyBlock(pos)) {
+			return false;
+		}
+
+		return paniclesBlock.defaultBlockState().canSurvive(level, pos);
+	}
+}
