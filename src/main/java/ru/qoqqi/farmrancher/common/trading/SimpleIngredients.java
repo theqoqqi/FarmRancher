0e93f39f303520fdec19bfc683b0cd5e4469022a
Qoqqi <sazykin.georgii@gmail.com>
2023-11-03 01:54:37 +0300

    feat: Сбалансированы цены простых ингредиентов

--- src/main/java/ru/qoqqi/farmrancher/common/trading/SimpleIngredients.java
+++ src/main/java/ru/qoqqi/farmrancher/common/trading/SimpleIngredients.java
@@ -12,26 +12,26 @@ public class SimpleIngredients {
 
 	public static final Map<Item, Double> PRICES = new HashMap<>() {
 		{
-			put(Items.WHEAT, per64Items(3));
-			put(Items.CARROT, per64Items(5));
-			put(Items.POTATO, per64Items(5));
-			put(Items.BEETROOT, per64Items(6));
-			put(Items.PUMPKIN, per64Items(20));
+			put(Items.WHEAT, per64Items(2.5));
+			put(Items.CARROT, per64Items(4));
+			put(Items.POTATO, per64Items(4.5));
+			put(Items.BEETROOT, per64Items(5));
+			put(Items.PUMPKIN, per64Items(8));
 			put(ModItems.PUMPKIN_SLICE.get(), get(Items.PUMPKIN) / 4);
-			put(Items.MELON, per64Items(32));
+			put(Items.MELON, per64Items(8));
 			put(Items.MELON_SLICE, get(Items.MELON) / 9);
-			put(ModItems.CABBAGE.get(), per64Items(5));
+			put(ModItems.CABBAGE.get(), per64Items(6));
 			put(ModItems.CABBAGE_LEAF.get(), get(ModItems.CABBAGE.get()) / 2);
-			put(ModItems.TOMATO.get(), per64Items(6));
+			put(ModItems.TOMATO.get(), per64Items(3.5));
 			put(ModItems.ONION.get(), per64Items(4));
-			put(ModItems.RICE.get(), per64Items(3));
+			put(ModItems.RICE.get(), per64Items(4.5));
 
 			put(Items.RED_MUSHROOM, per64Items(8));
 			put(Items.BROWN_MUSHROOM, per64Items(5));
-			put(Items.KELP, per64Items(5));
+			put(Items.KELP, per64Items(1.5));
 			put(Items.APPLE, per64Items(5));
-			put(Items.SUGAR, per64Items(2));
-			put(Items.EGG, per16Items(2));
+			put(Items.SUGAR, per64Items(1.5));
+			put(Items.EGG, per16Items(1.5));
 			put(Items.INK_SAC, per64Items(10));
 			put(Items.COCOA_BEANS, per64Items(4));
 			put(Items.BONE, per64Items(8));
@@ -40,15 +40,15 @@ public class SimpleIngredients {
 			put(Items.HONEY_BOTTLE, per16Items(2.5));
 			put(ModItems.MILK_BOTTLE.get(), per16Items(0.75));
 
-			put(Items.CHICKEN, per64Items(5));
+			put(Items.CHICKEN, per64Items(3.5));
 			put(ModItems.CHICKEN_CUTS.get(), get(Items.CHICKEN) / 2);
 			put(Items.BEEF, per64Items(5));
 			put(ModItems.MINCED_BEEF.get(), get(Items.BEEF) / 2);
-			put(Items.PORKCHOP, per64Items(5));
+			put(Items.PORKCHOP, per64Items(4));
 			put(ModItems.BACON.get(), get(Items.PORKCHOP) / 2);
 			put(ModItems.HAM.get(), get(Items.PORKCHOP) * 2);
-			put(Items.RABBIT, per64Items(8));
-			put(Items.MUTTON, per64Items(5));
+			put(Items.RABBIT, per64Items(6));
+			put(Items.MUTTON, per64Items(4.5));
 			put(ModItems.MUTTON_CHOPS.get(), get(Items.MUTTON) / 2);
 			put(Items.TROPICAL_FISH, per64Items(10));
 			put(Items.COD, per64Items(8));
