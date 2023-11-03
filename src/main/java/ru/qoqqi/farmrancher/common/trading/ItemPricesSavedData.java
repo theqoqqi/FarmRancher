4e3fdb42ed288a9204fa61923312b950f4d66b22
Qoqqi <sazykin.georgii@gmail.com>
2023-11-03 17:25:37 +0300

    feat: Добавлен ItemPricesSavedData для хранения информации о ценах на товары

---
+++ src/main/java/ru/qoqqi/farmrancher/common/trading/ItemPricesSavedData.java
@@ -0,0 +1,129 @@
+package ru.qoqqi.farmrancher.common.trading;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.saveddata.SavedData;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import javax.annotation.Nonnull;
+
+import it.unimi.dsi.fastutil.objects.Object2DoubleMap;
+import it.unimi.dsi.fastutil.objects.Object2DoubleOpenHashMap;
+
+public class ItemPricesSavedData extends SavedData {
+
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	public static final String NAME = "ItemPrices";
+
+	private final Object2DoubleMap<Item> prices = new Object2DoubleOpenHashMap<>();
+
+	private final ServerLevel level;
+
+	public ItemPricesSavedData(ServerLevel level) {
+		super();
+
+		this.level = level;
+	}
+
+	public ItemPricesSavedData(ServerLevel level, @Nonnull CompoundTag nbt) {
+		super();
+
+		this.level = level;
+
+		read(nbt);
+	}
+
+	public void read(@Nonnull CompoundTag nbt) {
+		LOGGER.info("READ PRICES");
+		readPrices(nbt);
+	}
+
+	private void readPrices(@Nonnull CompoundTag nbt) {
+		var list = nbt.getList("Prices", Tag.TAG_COMPOUND);
+
+		prices.clear();
+
+		var itemRegistry = getItemRegistry();
+
+		for (int i = 0; i < list.size(); i++) {
+			var entry = list.getCompound(i);
+			var item = itemRegistry.get(new ResourceLocation(entry.getString("Item")));
+			var price = entry.getDouble("Price");
+
+			if (item != null) {
+				prices.put(item, price);
+			}
+		}
+	}
+
+	@Nonnull
+	@Override
+	public CompoundTag save(@Nonnull CompoundTag nbt) {
+		LOGGER.info("SAVE PRICES");
+		writePrices(nbt);
+
+		return nbt;
+	}
+
+	private void writePrices(@Nonnull CompoundTag nbt) {
+		var list = new ListTag();
+
+		var itemRegistry = getItemRegistry();
+
+		prices.forEach((item, price) -> {
+			var entry = new CompoundTag();
+			var itemKey = itemRegistry.getKey(item);
+
+			if (itemKey != null) {
+				entry.putString("Item", itemKey.toString());
+				entry.putDouble("Price", price);
+
+				list.add(entry);
+			}
+		});
+
+		nbt.put("Prices", list);
+	}
+
+	@NotNull
+	private Registry<Item> getItemRegistry() {
+		return level.registryAccess().registry(Registries.ITEM).orElseThrow();
+	}
+
+	public boolean hasPrice(Item item) {
+		return prices.containsKey(item);
+	}
+
+	public double getPrice(Item item) {
+		return prices.getDouble(item);
+	}
+
+	public void removePrice(Item item) {
+		prices.removeDouble(item);
+		setDirty();
+	}
+
+	public void setPrice(Item item, float price) {
+		prices.put(item, price);
+		setDirty();
+	}
+
+	public static ItemPricesSavedData getInstance(ServerLevel level) {
+		return level.getDataStorage().computeIfAbsent(
+				nbt -> new ItemPricesSavedData(level, nbt),
+				() -> new ItemPricesSavedData(level),
+				NAME
+		);
+	}
+}
