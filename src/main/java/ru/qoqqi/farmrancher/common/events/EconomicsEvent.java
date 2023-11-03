edbd667c105d084b78064fa4889af592d3c59ba8
Qoqqi <sazykin.georgii@gmail.com>
2023-11-03 22:31:14 +0300

    feat: Добавлена группа событий EconomicsEvent

---
+++ src/main/java/ru/qoqqi/farmrancher/common/events/EconomicsEvent.java
@@ -0,0 +1,49 @@
+package ru.qoqqi.farmrancher.common.events;
+
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.level.LevelEvent;
+
+import ru.qoqqi.farmrancher.common.trading.Sellable;
+
+public class EconomicsEvent extends LevelEvent {
+
+	public EconomicsEvent(Level level) {
+		super(level);
+	}
+
+	public static class PriceEvent extends EconomicsEvent {
+
+		private final Sellable sellable;
+
+		private final double price;
+
+		public PriceEvent(Level level, Sellable sellable, double price) {
+			super(level);
+
+			this.sellable = sellable;
+			this.price = price;
+		}
+
+		public Sellable getSellable() {
+			return sellable;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+	}
+
+	public static class PriceAdded extends PriceEvent {
+
+		public PriceAdded(Level level, Sellable sellable, double price) {
+			super(level, sellable, price);
+		}
+	}
+
+	public static class PriceUpdated extends PriceEvent {
+
+		public PriceUpdated(Level level, Sellable sellable, double price) {
+			super(level, sellable, price);
+		}
+	}
+}
