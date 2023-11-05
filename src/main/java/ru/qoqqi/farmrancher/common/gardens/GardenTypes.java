81db2d4cc1894860093cd2d9a08e5411f2e7ed21
Qoqqi <sazykin.georgii@gmail.com>
2023-11-05 08:54:27 +0300

    balance: Переработаны характеристики садов

--- src/main/java/ru/qoqqi/farmrancher/common/gardens/GardenTypes.java
+++ src/main/java/ru/qoqqi/farmrancher/common/gardens/GardenTypes.java
@@ -8,15 +8,15 @@ public class GardenTypes {
 
 	private static final Logger LOGGER = LogUtils.getLogger();
 
-	public static final GardenType WOODEN_GARDEN = new GardenType(2, 0.8f, 0.8f);
+	public static final GardenType WOODEN_GARDEN = new GardenType(2, 0.8f, 0.9f);
 
 	public static final GardenType COPPER_GARDEN = new GardenType(3, 1f, 1f);
 
-	public static final GardenType IRON_GARDEN = new GardenType(3, 1f, 1f);
+	public static final GardenType IRON_GARDEN = new GardenType(4, 1.2f, 1.1f);
 
-	public static final GardenType GOLDEN_GARDEN = new GardenType(4, 1.2f, 1.1f);
+	public static final GardenType GOLDEN_GARDEN = new GardenType(5, 1.4f, 1.2f);
 
-	public static final GardenType DIAMOND_GARDEN = new GardenType(5, 1.5f, 1.25f);
+	public static final GardenType DIAMOND_GARDEN = new GardenType(6, 1.6f, 1.3f);
 
-	public static final GardenType NETHERITE_GARDEN = new GardenType(6, 2.0f, 1.5f);
+	public static final GardenType NETHERITE_GARDEN = new GardenType(7, 1.8f, 1.4f);
 }
