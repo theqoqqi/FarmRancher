b170535a447b60c4769e124fdd24033df902410f
Qoqqi <sazykin.georgii@gmail.com>
2023-11-06 21:16:39 +0300

    fix: Исправлена ошибка, из-за которой некорректно отображались многострочные подсказки у садовых блоков при большом размере окна игры

--- src/main/java/ru/qoqqi/farmrancher/common/items/GardenBlockItem.java
+++ src/main/java/ru/qoqqi/farmrancher/common/items/GardenBlockItem.java
@@ -1,8 +1,9 @@
 package ru.qoqqi.farmrancher.common.items;
 
+import com.mojang.logging.LogUtils;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -11,6 +12,7 @@ import net.minecraft.world.level.block.Block;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
 import java.util.List;
 
@@ -18,6 +20,8 @@ import ru.qoqqi.farmrancher.common.blocks.GardenBlock;
 import ru.qoqqi.farmrancher.common.gardens.GardenType;
 
 public class GardenBlockItem extends BlockItem {
+
+	private static final Logger LOGGER = LogUtils.getLogger();
 
 	public GardenBlockItem(Block block, Properties properties) {
 		super(block, properties);
@@ -32,19 +36,37 @@ public class GardenBlockItem extends BlockItem {
 	) {
 		var gardenBlock = (GardenBlock) getBlock();
 
-		tooltip.add(getGardenInfo(gardenBlock.gardenType).withStyle(ChatFormatting.GRAY));
+		tooltip.addAll(getGardenInfo(gardenBlock.gardenType));
 	}
 
-	private static MutableComponent getGardenInfo(GardenType gardenType) {
+	private static List<Component> getGardenInfo(GardenType gardenType) {
 		var areaSize = gardenType.areaRadius * 2 + 1;
 		var growthSpeed = Math.round(gardenType.growthSpeed * 100);
 		var profitability = Math.round(gardenType.profitability * 100);
 
-		return Component.translatable(
-				"block.farm_rancher.gardens.desc",
-				areaSize,
-				growthSpeed,
-				profitability
+		return List.of(
+				translatable(
+						"block.farm_rancher.gardens.desc1",
+						intLiteral(areaSize)
+				),
+				translatable(
+						"block.farm_rancher.gardens.desc2",
+						intLiteral(growthSpeed)
+				),
+				translatable(
+						"block.farm_rancher.gardens.desc3",
+						intLiteral(profitability)
+				)
 		);
+	}
+
+	private static Component translatable(String key, Object... args) {
+		return Component.translatable(key, args)
+				.withStyle(ChatFormatting.GRAY);
+	}
+
+	private static Component intLiteral(int value) {
+		return Component.literal(String.valueOf(value))
+				.withStyle(ChatFormatting.WHITE);
 	}
 }
