7b7459f704070b0cf8a05f74330d6127a35ce138
Qoqqi <sazykin.georgii@gmail.com>
2023-11-06 23:29:27 +0300

    feat: Теперь если забирать предметы в торговом диалоге с зажатым Shift, необходимые для повторной торговли предметы автоматически помещаются в слот

--- src/main/java/ru/qoqqi/farmrancher/client/menus/FixedMerchantMenu.java
+++ src/main/java/ru/qoqqi/farmrancher/client/menus/FixedMerchantMenu.java
@@ -1,16 +1,52 @@
 package ru.qoqqi.farmrancher.client.menus;
 
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MerchantMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.trading.Merchant;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
 public class FixedMerchantMenu extends MerchantMenu {
+
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	private int selectedOffer;
 
 	public FixedMerchantMenu(int containerId, Inventory playerInventory, Merchant merchant) {
 		super(containerId, playerInventory, merchant);
+	}
+
+	@NotNull
+	@Override
+	public ItemStack quickMoveStack(@NotNull Player player, int index) {
+		var itemStack = super.quickMoveStack(player, index);
+
+		if (index == RESULT_SLOT) {
+			Minecraft.getInstance().tell(this::reselectOffer);
+		}
+
+		return itemStack;
+	}
+
+	private void reselectOffer() {
+		setSelectionHint(selectedOffer);
+		tryMoveItems(selectedOffer);
+	}
+
+	@Override
+	public void setSelectionHint(int offerIndex) {
+		super.setSelectionHint(offerIndex);
+
+		selectedOffer = offerIndex;
 	}
 
 	@Override
